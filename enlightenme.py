247a473e65449f04c2f7559bcf0f2913d0dd2a08
git-ankitgautam <ankitgautam485@gmail.com>
2024-09-21 00:30:56 +0530

    changed enlightenme command from plain text to embed format

--- enlightenme.py
+++ enlightenme.py
@@ -1,11 +1,17 @@
 import requests
 import json
+import discord
 
 def get_quote():
-  zenquote = requests.get("https://zenquotes.io/api/random")
-  zenquote_json = json.loads(zenquote.text)
-  data = {}
-  data["quote"] = zenquote_json[0]['q']
-  data["author"] = zenquote_json[0]['a']
-  
-  return data
+	zenquote = requests.get("https://zenquotes.io/api/random")
+	zenquote_json = json.loads(zenquote.text)
+	data = {}
+	data["quote"] = zenquote_json[0]['q']
+	data["author"] = zenquote_json[0]['a']
+
+	embedvar = discord.Embed()
+
+	embedvar.add_field(name="",value=data["quote"], inline=False)
+	embedvar.add_field(name="",value=f"-{data["author"]}", inline=False)
+
+	return embedvar
