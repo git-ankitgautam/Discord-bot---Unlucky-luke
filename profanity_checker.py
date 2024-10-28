ae32005469b76f4e1e1afe12606780a6e3ccdea6
git-ankitgautam <ankitgautam485@gmail.com>
2024-10-28 12:06:23 +0530

    don't need a seperate func to implement regex

--- profanity_checker.py
+++ profanity_checker.py
@@ -1,13 +1,8 @@
 import re
 
-def clean_and_tokenize(input_string):
-    # Convert to lowercase and use regex to find all word sequences (ignores punctuation)
-    words = re.findall(r'\b\w+\b', input_string.lower())
-    return words
-
-
 def profanity_check(search_string):
-    cleaned_string = clean_and_tokenize(search_string)
+    # Convert to lowercase and use regex to find all word sequences (ignores punctuation)
+    cleaned_string = re.findall(r'\b\w+\b', search_string.lower())
     with open("profanity_wordslist.txt") as file:
         document_Text = file.read()
         for word in document_Text.splitlines():
